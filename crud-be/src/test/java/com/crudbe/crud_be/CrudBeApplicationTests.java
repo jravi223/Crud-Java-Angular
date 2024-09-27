3be624ef481b27ac0c68582f6cc91d103cf28a34
Ravi J <jravi223@gmail.com>
2024-09-27 21:16:50 +0530

    Remote repo

---
+++ crud-be/src/test/java/com/crudbe/crud_be/CrudBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudbe.crud_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
